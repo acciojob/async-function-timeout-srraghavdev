83ddafc18faaaf66c545386663aa33c1278bfa4b
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-06-27 15:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const textref= document.getElementById("text")
+const numberref= document.getElementById("delay")
+const button = document.getElementById("btn")
+button.addEventListener("click",delayfunc)
+async function delayfunc(){
+	let textval = textref.value
+	let delayval= numberref.value
+	await new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve()
+		},delayval)
+	})
+	document.getElementById("output").innerText=textval
+}
